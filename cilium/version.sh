623a898e591e1396edec2812171db5abda3cc0d6
weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>
2025-07-10 17:22:10 +0800

    cilium v1.17.5
    Signed-off-by: weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>

--- cilium/version.sh
+++ cilium/version.sh
@@ -2,18 +2,18 @@
 
 # !!!!!!!!!!
 # https://github.com/cilium/cilium/releases
-CILIUM_VERSION=${CILIUM_VERSION:-"1.17.3"}
+CILIUM_VERSION=${CILIUM_VERSION:-"1.17.5"}
 
 # v1.17.0 支持 gatewayAPI v1.2.0 
 # https://docs.cilium.io/en/latest/network/servicemesh/gateway-api/gateway-api/
 GATEWAY_API_VERSION=${GATEWAY_API_VERSION:-""v1.2.0""}
 
 # https://github.com/cilium/cilium-cli/releases
-CILIUM_CLI_VERSION=${CILIUM_CLI_VERSION:-"v0.18.3"}
+CILIUM_CLI_VERSION=${CILIUM_CLI_VERSION:-"v0.18.5"}
 
 # !!!!!!!!!!
 # https://github.com/cilium/hubble/releases
-HUBBLE_CLI_VERSION=${HUBBLE_CLI_VERSION:-"v1.17.2"}
+HUBBLE_CLI_VERSION=${HUBBLE_CLI_VERSION:-"v1.17.5"}
 
 # https://github.com/cilium/tetragon/releases
 TETRAGON_VERSION=${TETRAGON_VERSION:-"1.4.0"}
